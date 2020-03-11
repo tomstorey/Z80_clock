e63c0f576bd45a38a38dd8e152c7f65ced6f1d91
Tom Storey <7754310+tomstorey@users.noreply.github.com>
2020-03-11 21:01:36 +1100

    Changes to loader.py
    Wait for acknowledgements from the monitor that actions have completed.

--- loader.py
+++ loader.py
@@ -71,25 +71,85 @@ def main():
 
     data = bytes(data)
 
-    start = time.time()
-
     ser = Serial(
         '/dev/cu.usbserial-FT94JP1H',
         baudrate=57600,
-        timeout=5
+        timeout=1
     )
+
+    if ser.in_waiting > 0:
+        ser.read(size=ser.in_waiting)
+
+    # Wait for serial loader to be available
+    print('Waiting for serial loader availability: ', end='', flush=True)
+
+    failed = 0
+
+    while True:
+        ser.write(bytes([0x01]))
+        ser.flush()
+
+        try:
+            if ord(ser.read(size=1)) == 64:
+                print(' OK')
+                break
+        except TypeError:
+            failed += 1
+
+            print('.', end='', flush=True)
+
+            if failed == 10:
+                print(' Failed after too many attempts')
+
+                return
+
+    start = time.time()
+
+    print('Transferring %d bytes to 0x%4.4X: ' % (filesize, base), end='', flush=True)
+
     ser.write(data)
     ser.flush()
 
+    failed = 0
+
+    while True:
+        try:
+             if ord(ser.read(size=1)) == 33:
+                break
+        except TypeError:
+            failed += 1
+
+            print('.', end='', flush=True)
+
+            if failed == 5:
+                print(' Failed: transfer not acknowledged')
+
+                return
+
     duration = time.time() - start
 
-    if ord(ser.read(size=1)) == 33:
-        print('\nTransferred %d bytes to 0x%4.4X in %.3fs\n' % (filesize, base, duration))
+    print(' Done in %.3fs' % duration)
 
+    failed = 0
 
     if exec is True:
-        if ord(ser.read(size=1)) == 43:
-            print('Executing from 0x%4.4X\n' % base)
+        print('Executing from 0x%4.4X: ' % base, end='', flush=True)
+
+        while True:
+            try:
+                if ord(ser.read(size=1)) == 43:
+                    break
+            except TypeError:
+                failed += 1
+
+                print('.', end='', flush=True)
+
+                if failed == 2:
+                    print(' Failed: execution not acknowledged')
+
+                    return
+
+        print(' OK')
 
     ser.close()
 
